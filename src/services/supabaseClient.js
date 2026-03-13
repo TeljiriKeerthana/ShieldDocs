1799540b0097b8a451078986d9a51648e04b486d
Keerthana <teljirikeerthana24@gmail.com>
2026-03-13 12:14:47 +0530

    changed variables

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://pjokgfpmbhjtjlmcmtiz.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqb2tnZnBtYmhqdGpsbWNtdGl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMjQ1NjMsImV4cCI6MjA4ODkwMDU2M30.HOMRQUNOHhKFXOZR5H80Pe-xiRgZ0XiujTq-4TCebH0"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://pjokgfpmbhjtjlmcmtiz.supabase.co"
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqb2tnZnBtYmhqdGpsbWNtdGl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMjQ1NjMsImV4cCI6MjA4ODkwMDU2M30.HOMRQUNOHhKFXOZR5H80Pe-xiRgZ0XiujTq-4TCebH0"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
